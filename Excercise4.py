afe6d0a9e145d7795a83c467c588fd23ad517bb2
shinedesigners <shinedesigners@yahoo.com>
2018-06-13 04:27:17 +0530

    Create Excercise4.py
    This how we can display two lists using for loop

---
+++ Excercise4.py
@@ -0,0 +1,5 @@
+months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+who_born = ['James', 'Fiona', 'Marshal', 'Apollo', 'May', 'Jade', 'John', 'Antoly', 'Steven', 'Ontario', 'Nubitel', 'Doug']
+
+for month,who in zip(months,who_born):
+	print(who,month)
